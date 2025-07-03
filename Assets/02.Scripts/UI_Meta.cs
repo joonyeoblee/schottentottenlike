519d2b54f816d6d0504051c13c31871be1dc56af
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-03 21:07:06 +0900

    Fix Photon Server Settings Fixed Region 수정
    빌드 후 테스트할 때 서로 다른 리전이 나와 만나지 못하는 문제 해결함. Fixe Region을 kr로 고정시킴.

--- Assets/02.Scripts/UI_Meta.cs
+++ Assets/02.Scripts/UI_Meta.cs
@@ -1,10 +1,21 @@
+using System;
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UI_Meta : MonoBehaviour
 {
+    public TextMeshProUGUI Text;
     public void RetrunToLobby()
     {
         SceneManager.LoadScene(1);
     }
+
+    private void Start()
+    {
+        Debug.Log($"클라우드 리전: {PhotonNetwork.CloudRegion}");
+        Text.text = $"{PhotonNetwork.CloudRegion}";
+    }
+
 }
